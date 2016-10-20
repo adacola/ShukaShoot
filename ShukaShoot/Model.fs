f81cf1f9d4c13c2c422e1df459008a7441c238c8
adacola <adacola255@gmail.com>
2016-10-20 13:27:48 +0900

    認証情報以外の設定も設定ファイルに記載するように変更。警察の実装を別ファイルに分けた。

---
+++ ShukaShoot/Model.fs
@@ -0,0 +1,7 @@
+﻿module Adacola.ShukaShoot.Model
+
+open FSharp.Data
+
+type Config = JsonProvider<"configSample.json">
+let private configFileName = "config.json"
+let config = Config.Load(configFileName)
--- ShukaShoot/Police.fs
+++ ShukaShoot/Police.fs
@@ -1,50 +1,9 @@
-module Adacola.ShukaShoot.Police
+﻿module Adacola.ShukaShoot.Police
 
-open System
 open System.Text.RegularExpressions
-
-let private random = Random()
-let private waitIntervals = [|None; Some 1; Some 3; Some 8; Some 13; Some 21; Some 34; Some 55; Some 89; Some 144|]
-let private shukaUserID = 215805231L
-
-let activeTime = TimeSpan.FromMinutes(60.0)
-let coffeeActiveTime = TimeSpan.FromMinutes(5.0)
-
-let getFavoriteWaitSecond() =
-    waitIntervals.[random.Next waitIntervals.Length]
-
-let favorite tokens statusID =
-    async {
-        match getFavoriteWaitSecond() with
-        | Some second ->
-            do Console.WriteLine("{0} 秒待機", second)
-            do! Async.Sleep (second * 1000)
-            let! status = Twitter.favoriteTweet tokens statusID
-            return Some(status)
-        | None ->
-            return None
-    }
-
-let isFavoriteText text =
-    Regex.IsMatch(text, @"((し|シ)\s*(ゅ|ゆ|ュ|ユ)\s*(か|カ|力)\s*((し|シ)\s*(ゅ|ゆ|ュ|ユ)\s*(ー|う|ぅ|ウ|ゥ)|(ぴ|ピ))|(?<!#)朱夏)(?!警察)") && not (Regex.IsMatch(text, @"(\s|\A)@Saito_Shuka(\s|\Z)"))
 
 let isSuspendText text =
     Regex.IsMatch(text, @"(落ち(る|ります|よう?)|電源切(る|ります|ろう?)|寝(る|ます|よう?))$")
-
-let isStrictCoffee text =
-    Regex.IsMatch(text, @"\Aコーヒーこぼした\Z")
-
-let isCoffee text =
-    Regex.IsMatch(text, @"コーヒー.*(こぼし|零し|溢し)(ちゃっ|てしまっ|ちまっ)?(た|て)") && not (Regex.IsMatch(text, @"(\s|\A)@(anju_inami|Rikako_Aida|suwananaka|box_komiyaarisa|Saito_Shuka|Aikyan_|Kanako_tktk|aina_suzuki723|furihata_ai|adacola)(\s|\Z)", RegexOptions.IgnoreCase))
-
-let (|FavoriteTweet|_|) = function
-    | Twitter.MyTweet(_) -> None
-    | Twitter.Tweet(m) -> 
-        match m with
-        | Twitter.Retweet(_) -> None
-        | _ when isFavoriteText m.Status.Text && not (Twitter.isReplyTo shukaUserID m) -> Some(m, m.Status.Id)
-        | _ -> None
-    | _ -> None
 
 let (|SuspendTweet|_|) = function
     | Twitter.MyTweet(m) ->
@@ -54,14 +13,6 @@ let (|SuspendTweet|_|) = function
         | _ -> None
     | _ -> None
 
-let (|CoffeeTweet|_|) = function
-    | Twitter.Tweet(m) when isCoffee m.Status.Text -> Some(m, m.Status.Id)
-    | _ -> None
-
-let (|StrictCoffeeTweet|_|) = function
-    | Twitter.Tweet(m) -> 
-        match m with
-        | Twitter.Retweet(_) -> None
-        | _ when isStrictCoffee m.Status.Text -> Some(m, m.Status.Id)
-        | _ -> None
+let (|StartTweet|_|) = function
+    | Twitter.MyTweet(m) -> Some(m)
     | _ -> None
